06ccef09994d55b368595413ef47b187f28b94ff
st1803 <st1803>
2020-10-13 11:23:17 +0300

    Add project

---
+++ Tourapps/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourapps
+{
+    class Manager
+    {
+        public static System.Windows.Controls.Frame MainFrame { get; set; }
+    }
+}
